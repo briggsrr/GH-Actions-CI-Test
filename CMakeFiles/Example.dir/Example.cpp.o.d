db795d176424c5702180f7604872c1ee74d3adfd
briggsrr <robert.briggs.3@icloud.com>
2022-07-29 14:36:37 -0700

    Added git ignore

--- CMakeFiles/Example.dir/Example.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Example.dir/Example.cpp.o: \
-  /Users/bobbybriggs/Desktop/Software-Creations/GH-Actions-CI-Test/Example.cpp \
-  /Users/bobbybriggs/Desktop/Software-Creations/GH-Actions-CI-Test/Example.h
